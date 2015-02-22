0c48ebfdce27726587ae6797643b29b7fe0b99f7
Hiroaki Nakamura <hnakamur@gmail.com>
2015-02-23 00:04:38 +0900

    Fix 32bit VariantToUintptrArray

--- examples/runcalc/main.go
+++ examples/runcalc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/hnakamur/w32uiautomation"
@@ -38,36 +39,16 @@ func runCalc() error {
 	if err != nil {
 		return err
 	}
-	found, err := w32uiautomation.WaitFindFirst(root, w32uiautomation.TreeScope_Children, condition)
+	calc, err := w32uiautomation.WaitFindFirst(root, w32uiautomation.TreeScope_Children, condition)
 	if err != nil {
 		return err
 	}
 
-	foundName, err := found.Get_CurrentName()
+	calcName, err := calc.Get_CurrentName()
 	if err != nil {
 		return err
 	}
-	// I don't know why, but we get an empty string for foundName
-	//fmt.Printf("foundName=%v\n", foundName)
-
-	//foundAutomationId, err := found.Get_CurrentAutomationId()
-	//if err != nil {
-	//	return err
-	//}
-	//fmt.Printf("foundAutomationId=%v\n", foundAutomationId)
-
-	calc := found
-	//calc, err := w32uiautomation.FindFirstWithBreadthFirstSearch(auto, root,
-	//	w32uiautomation.NewElemMatcherFuncWithName(calculatorName))
-	//if err != nil {
-	//	return err
-	//}
-	//calcName, err := calc.Get_CurrentName()
-	//if err != nil {
-	//	return err
-	//}
-	//// NOTE: Here we can get the actual name, "Calculator"
-	//fmt.Printf("calc=%v, calcName=%v\n", calc, calcName)
+	fmt.Printf("calcName=%v\n", calcName)
 
 	pushButton(auto, calc, clearButtonAutomationId)
 	if err != nil {
@@ -105,37 +86,13 @@ func pushButton(auto *w32uiautomation.IUIAutomation, calc *w32uiautomation.IUIAu
 		return err
 	}
 
-	//button, err := w32uiautomation.WaitFindFirst(calc,
-	//	w32uiautomation.TreeScope_Subtree,
-	//	condition)
-	//fmt.Printf("pushButton button#1=%v\n", button)
-	//if err != nil {
-	//	return err
-	//}
-	// NOTE: I don't know why but I got an error if I call Invoke() with
-	// button returned by WaitFindFirst above.
-	//
-	// Invoke start. element=&{{0x7183c068}}
-	// Invoke. unknown=<nil>
-	// panic: runtime error: invalid memory address or nil pointer dereference
-	// [signal 0xc0000005 code=0x0 addr=0x0 pc=0x4539b0]
-	//err = w32uiautomation.Invoke(button)
-	//if err != nil {
-	//	return err
-	//}
-
-	button, err := w32uiautomation.WaitFindFirstWithBreadthFirstSearch(
-		auto, calc,
-		w32uiautomation.NewElemMatcherFuncWithAutomationId(automationId))
+	button, err := w32uiautomation.WaitFindFirst(calc,
+		w32uiautomation.TreeScope_Subtree,
+		condition)
 	if err != nil {
 		return err
 	}
-
-	err = w32uiautomation.Invoke(button)
-	if err != nil {
-		return err
-	}
-	return nil
+	return w32uiautomation.Invoke(button)
 }
 
 func main() {
